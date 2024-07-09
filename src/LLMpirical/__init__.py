0bfe95cb36ac4d6f4ea98d1875255b156a43977a
KyleAMoore <kylito1@gmail.com>
2024-07-09 13:31:25 -0500

    empty pypi boilerplate

---
+++ src/LLMpirical/__init__.py
@@ -0,0 +1,5 @@
+def not_implemented():
+    print('The LLMpirical library is a placeholder library that will be completed and published at a later date')
+
+if __name__=='__main__':
+    not_implemented()
